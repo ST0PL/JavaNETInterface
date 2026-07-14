6347d0b5d9d1bcdf01a14b5bcc186e942dcea630
st0pl <st0pl0gin@protonmail.com>
2026-07-14 13:47:57 +0700

    Add wrappers for List, Object and JObject pointer

---
+++ JavaNETInterface/Wrappers/JObjectPtr.cs
@@ -0,0 +1,9 @@
+﻿using JavaNETInterface.Jni;
+
+namespace JavaNETInterface.Wrappers
+{
+    public unsafe readonly struct JObjectPtr(nint v)
+    {
+        public readonly JObject* Value => (JObject*)v;
+    }
+}
