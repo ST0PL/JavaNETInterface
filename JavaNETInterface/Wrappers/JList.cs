315f7c76c161a91c9563e06a9dac219e82b42d21
st0pl <st0pl0gin@protonmail.com>
2026-07-14 14:32:21 +0700

    Change JList access modifier, move JListEnum into JList

--- JavaNETInterface/Wrappers/JList.cs
+++ JavaNETInterface/Wrappers/JList.cs
@@ -3,59 +3,58 @@ using System.Collections;
 
 namespace JavaNETInterface.Wrappers
 {
-    public unsafe class JListEnum(JniEnv* env, JObject* obj) : IEnumerator<JObjectPtr>
+
+    public unsafe class JList : JniObject, IEnumerable<JObjectPtr>
     {
-        private readonly JObject* _object = obj;
-        private readonly JniEnv* _env = env;
-
-        public JObject* Get(int index)
+        public unsafe class JListEnum(JniEnv* env, JObject* obj) : IEnumerator<JObjectPtr>
         {
-            JValue args = default;
-            args.I = index;
-            return _env->CallObjectMethodA(_object, JList.getMethodId, &args);
-        }
+            private readonly JObject* _object = obj;
+            private readonly JniEnv* _env = env;
 
-        public int GetSize()
-            => _env->CallIntMethod(_object, JList.sizeMethodId);
-
-        // IEnumerable implementations
-
-        private JObjectPtr _current;
-        public JObjectPtr Current => _current;
-        object IEnumerator.Current => Current;
-
-        private int position = -1;
-
-        public bool MoveNext()
-        {
-            if (position + 1 < GetSize())
+            public JObject* Get(int index)
             {
-                position++;
-                var jobj = Get(position);
-                _current = new JObjectPtr((nint)jobj);
-                return true;
+                JValue args = default;
+                args.I = index;
+                return _env->CallObjectMethodA(_object, getMethodId, &args);
             }
-            return false;
+
+            public int GetSize()
+                => _env->CallIntMethod(_object, sizeMethodId);
+
+            // IEnumerable implementations
+
+            private JObjectPtr _current;
+            public JObjectPtr Current => _current;
+            object IEnumerator.Current => Current;
+
+            private int position = -1;
+
+            public bool MoveNext()
+            {
+                if (position + 1 < GetSize())
+                {
+                    position++;
+                    var jobj = Get(position);
+                    _current = new JObjectPtr((nint)jobj);
+                    return true;
+                }
+                return false;
+            }
+
+            public void Reset()
+            {
+                position = -1;
+                _current = default;
+            }
+
+            public void Dispose()
+            { }
         }
-
-        public void Reset()
-        {
-            position = -1;
-            _current = default;
-        }
-
-        public void Dispose()
-        { }
-    }
-
-    internal unsafe class JList : JniObject, IEnumerable<JObjectPtr>
-    {
         public static JClass* Class { get; private set; }
 
-        public static JMethodID* getMethodId;
-        public static JMethodID* sizeMethodId;
-
         private static readonly Lock lockObject = new();
+        private static JMethodID* getMethodId;
+        private static JMethodID* sizeMethodId;
 
         private static bool initialized;
         public JList(JniEnv* env, JObject* obj) : base(env, obj)
@@ -81,11 +80,11 @@ namespace JavaNETInterface.Wrappers
             return _env->CallObjectMethodA(Object, getMethodId, &args);
         }
 
-        public IEnumerator<JObjectPtr> GetEnumerator()
-            => new JListEnum(_env, Object);
-
         public int GetSize()
             => _env->CallIntMethod(Object, sizeMethodId);
+
+        public IEnumerator<JObjectPtr> GetEnumerator()
+            => new JListEnum(_env, Object);
 
         IEnumerator IEnumerable.GetEnumerator()
             => GetEnumerator();
